bac0d125a1ca11d816c06daaa15f0884277924ab
carawang <carawang@redhat.com>
2019-05-26 20:37:12 +0800

    Add the parser and readme file
    No more explanation. :)

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# cucumber_duration_parser
-Based on the threshold, group your cucumber features automatically
+Cucumber HTML Report Duration Parser
+===
+
+Background
+====
+
+Nowadays, we build our CI/CD in the cloud platform. It is very common for us to build cucumber testing
+parallel to decrease the testing time and improve the efficiency.
+For example, in my project, I split the cucumber cases to multiple groups. Every group does testing
+against separate instant created apps. One pipeline can control the whole process, based on some parameters you specify to build apps, deploy them then do testings in parallel.
+
+One important thing is how to split the cucumber cases to reasonable groups.
+The parser is to resolve the problem.
+
+How it works
+===
+Shortly, we parser the 'overview-features.html' cucumber report to get the duration and features map, then based on the threshold user specifies, the parser would group the features automatically.
+For example, ideally, if all cases run in 300 seconds, we specify the threshold as 100, the parser
+would split them to 3 groups. The duration of each group is close to 100.
+
+How to use it
+===
+Python 3 and some basic python libs(you can see the file directly to confirm the packages) are needed. And you need to download your 'overview-features.html' cucumber report from your Jen`kins project, then try to run the following command:
+```
+python3 cucumber_feature_duration_parser.py --cucumber-html-report=overview-features.html
+--threshold=900
+```
+You will get the following output:
+```
+[features_time_parser.py::format_features_groups] INFO Complete: format the grouped features as below:
+{' -n "A light weight empty module push e2e case" -n "A simple example E2E test case" -n "Add "Product Security Reviewer" field" -n "Add ability to bulk remove builds in an advisory" -n "Add bug to advisory" -n "Add mixed types of builds in one same advisory" -n "Attach multi-arch container builds to advisories" -n "Batch" -n "Blacklist subpackages from RHN" -n "Brew" -n "Bug advisory eligibility" -n "CAT" -n "CDN Docker Push"': 1071, ' -n "Calculate \'reboot_suggested\' value based on packages" -n "Container Health Index Grade" -n "Edit advisory" -n "Embargoed Advisory dependency" -n "Errata and Bugzilla interactions" -n "Login" -n "Main stream release for RHEL product versions and variants" -n "Module builds on builds tab" -n "Multi-Product" -n "Older build and newer build of same package" -n "Product Listing features" -n "Product Listings" -n "Product Security Approval" -n "Provide option to 
+disable ACL (Approved Component List) requirement for releases" -n "Publish all erratum changes to UMB"': 1036, ' -n "Push multiple advisories in a batch" -n "RHSAs require a CPE" -n "RPM files should be shown on content page for RHEL-8 advisory" -n "RPM manifest in Container" -n "RPMDiff support for module builds" -n "Released build validation" -n "The policy for role buildroot" -n "The rule of setting released build" -n "bug eligibility checks for rhel8 bugs"': 811}
+```
+One key is one feature group.
+
+Free to enjoy it!
---
+++ cucumber_feature_duration_parser.py
@@ -0,0 +1,177 @@
+from bs4 import BeautifulSoup
+import re
+import itertools
+import sys
+import datetime
+import logging
+import argparse
+
+logging.basicConfig(
+    format='%(asctime)s [%(filename)s::%(funcName)s] %(levelname)s %(message)s',
+    level=logging.DEBUG
+)
+
+
+class CucumberFeaturesDurationParser(object):
+    """
+    One parser to group features based on the threshold
+    """
+
+    def __init__(self, html, threshold):
+        try:
+            self.soup = BeautifulSoup(open(html), features="lxml")
+            self.threshold = threshold
+        except Exception:
+            logging.info(
+                'Failed to open the cucumber htmnl report:{}'.format())
+
+    def __calculate_time(self, duration):
+        '''
+        return duration_seconds: int
+        '''
+        time_list = duration.split(' ')
+        real_time = 0
+        for time in time_list:
+            if time.find('m') > 0 and time.find('ms') < 0:
+                real_time += int(time.strip('m')) * 60
+            if time.find('s') > 0 and time.find('ms') < 0:
+                real_time += int(time.strip('s'))
+        return real_time
+
+    def __get_features_list_from_soap(self):
+        '''
+        return []: features_list
+        '''
+        logging.info('Get features list from the soap')
+        features_list = []
+        for feature in self.soup.find_all('a', href=re.compile('report-feature*')):
+            features_list.append(feature.text)
+        return features_list
+
+    def __get_time_list_from_soap(self):
+        '''
+        return []: time_list
+        '''
+        logging.info('Get time list from the soap')
+        time_list = []
+        for duration in self.soup.find_all('td', {'class': 'duration'}):
+            time = self.__calculate_time(duration.text)
+            time_list.append(time)
+        return time_list
+
+    def __get_whole_duration(self, feature_time_map):
+        '''
+        return int: the duration of the features dictionary
+        '''
+        total_duration = 0
+        logging.info('Begin: get the duration of the features')
+        for feature, time in feature_time_map.items():
+            total_duration += time
+        logging.info(
+            'Complete: the total duration is {}'.format(total_duration))
+        return total_duration
+
+    def get_features_durations_map(self):
+        '''
+        return {}: features_durations_map
+        '''
+        logging.info('Begin: get the features and durations map')
+        features_time_map = {}
+        try:
+            features_list = self.__get_features_list_from_soap()
+            time_list = self.__get_time_list_from_soap()
+            features_time_map = dict(zip(features_list, time_list))
+            logging.info('Complete: get the features and durations map')
+            logging.info(features_time_map)
+        except Exception:
+            logging.info('Fail to get features and durations map!')
+
+        return features_time_map
+
+    def spilt_features_to_groups_by_threshold(self, feature_time_map, time_threshold):
+        '''
+        return []: features groups list
+        '''
+        logging.info('Begin: spilt features {} to groups by the threshold {}'.format(
+            feature_time_map, time_threshold))
+        features_groups_list = []
+        total_time = 0
+        begin_index = 0
+        end_index = 0
+        for feature, time in feature_time_map.items():
+            if total_time < int(time_threshold):
+                total_time += time
+            else:
+                end_index = list(feature_time_map.keys()).index(feature)
+                feature_time_map_featuers = list(feature_time_map.keys())[
+                    begin_index:end_index]
+                feature_time_map_time = list(feature_time_map.values())[
+                    begin_index:end_index]
+                features_groups_list.append(
+                    dict(zip(feature_time_map_featuers, feature_time_map_time)))
+                begin_index = end_index
+                total_time = 0
+
+        ungrouped_feature_time_map_features = list(
+            feature_time_map.keys())[begin_index:]
+        ungrouped_feature_time_map_time = list(
+            feature_time_map.values())[begin_index:]
+        features_groups_list.append(
+            dict(zip(ungrouped_feature_time_map_features, ungrouped_feature_time_map_time)))
+        logging.info('Complete: the grouped features are ready: \n{}'.format(
+            features_groups_list))
+        return features_groups_list
+
+    def format_features_groups(self, features_groups_list):
+        '''
+        return {}: {'-n feature..': duration, ...}
+        '''
+        logging.info(
+            'Begin: format the grouped features and durations with cucumber ops "-n"')
+        formmated_features_groups = {}
+        for features_group in features_groups_list:
+            features_string = ''
+            total_time = 0
+            for feature, time in features_group.items():
+                features_string += ' -n "{}"'.format(feature)
+                total_time += time
+            formmated_features_groups[features_string] = total_time
+
+        logging.info('Complete: format the grouped features as below:\n{}'.format(
+            formmated_features_groups))
+        return formmated_features_groups
+
+    def confirm_the_grouped_result(self, formmated_features_groups, feature_time_map):
+        '''
+        return 0/1
+        '''
+        total_time = 0
+        logging.info(
+            'Begin: compare the duration the new grouped features with the original features')
+        if self.__get_whole_duration(formmated_features_groups) == self.__get_whole_duration(feature_time_map):
+            logging.info(
+                'Complete: features are grouped successfully, Cheers!')
+            return 0
+        else:
+            logging.info('Complete: features are failed to be grouped, Sorry!')
+            return 1
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cucumber-html-report',
+                        help='The cucumber html report path')
+    parser.add_argument(
+        '--threshold', help='The threshold of the duration of each feature group')
+    args = parser.parse_args()
+    cucumber_html_report = args.cucumber_html_report
+    threshold = args.threshold
+    cucumber_report_duration_parser = CucumberFeaturesDurationParser(
+        cucumber_html_report, threshold)
+    feature_time_map = cucumber_report_duration_parser.get_features_durations_map()
+    features_groups_list = cucumber_report_duration_parser.spilt_features_to_groups_by_threshold(
+        feature_time_map, threshold)
+    formmated_features_groups = cucumber_report_duration_parser.format_features_groups(
+        features_groups_list)
+    cucumber_report_duration_parser.confirm_the_grouped_result(
+        formmated_features_groups, feature_time_map)
